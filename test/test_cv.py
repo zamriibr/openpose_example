1c4ceb9df2af80027f5ac9a10f637aa7a9bf6b5a
zamriibr <48670145+zamriibr@users.noreply.github.com>
2020-10-15 00:44:51 +0800

    Add files via upload

---
+++ test/test_cv.py
@@ -0,0 +1,8 @@
+import numpy as np
+#import matplotlib.pyplot as plt
+import cv2
+
+img = cv2.imread('zidane.jpg')    #Make sure you have the image
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
